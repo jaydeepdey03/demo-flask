c121b063d4f232f152ac8f1426567b39d25fb512
Jaydeep <jaydeep.dey03@gmail.com>
2023-01-07 08:29:03 +0530

    Modified 2

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/hello')
+def hello_world():
+    return 'Hello, Jaydeep!'
+
 if __name__ == "__main__":
     app.run(debug=True)
     # serve(app, port=5000)
