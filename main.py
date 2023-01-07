65724e15ef90d23b4427d927512b8f64c5d64828
Jaydeep <jaydeep.dey03@gmail.com>
2023-01-07 09:41:55 +0530

    Imported ML Model from another repo

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask import Flask, jsonify, request
+import pandas,pickle
+from Controllers import transformText
+from flask_cors import CORS
 # from waitress import serve
 app = Flask(__name__)
+
+# Handling CORS
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+# importing models
+model = pickle.load(open('./model/model.pkl', 'rb'))
+tfidf = pickle.load(open('./model/vectorizer.pkl', 'rb'))
 
 @app.route('/')
 def hello_world():
@@ -9,6 +21,17 @@ def hello_world():
 @app.route('/hello')
 def hey():
     return 'Hello, Jaydeep!'
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    transformed_sms = transformText.transform_text(data['sentence'])
+    vector_input = tfidf.transform([transformed_sms])
+    result = model.predict(vector_input)[0]
+    return jsonify({'result': int(result)})
+    # print(data)
+    # return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
